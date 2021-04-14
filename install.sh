5442820bac1a333dad24e796c118b38c77aed363
Ren Zeng <38289304+zerollzeng@users.noreply.github.com>
2021-04-14 18:45:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-wget -O - https://raw.githubusercontent.com/zerollzeng/config/master/.bashrc >> ~/.bashrc
-source ~/.bashrc
-wget -O - https://raw.githubusercontent.com/zerollzeng/config/master/.vimrc > ~/.vimrc
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
+~/.fzf/install && \
+wget -O - https://raw.githubusercontent.com/zerollzeng/config/master/.bashrc >> ~/.bashrc && \
+source ~/.bashrc && \
+wget -O - https://raw.githubusercontent.com/zerollzeng/config/master/.vimrc > ~/.vimrc && \
